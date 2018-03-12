3548fa809de005a271c277f89ef9afebdf2016fe
yanlele <331393627@qq.com>
2018-03-12 22:19:29 +0800

    实例化Module的构造方法，让module来解析我们内存获得的bundle

--- server/util/dev-static.js
+++ server/util/dev-static.js
@@ -13,6 +13,8 @@ const getTemplate=()=>{
     })
 };
 
+const Module = module.constructor;//module的构造方法，可以通过这个构造方法创建一个新的module
+
 const mfs = new MemoryFs;//实例化MemoryFs
 const serverCompiler = webpack(serverConfig);//启动一个webpack
 serverConfig.outputFileSystem = mfs;
@@ -34,6 +36,8 @@ serverCompiler.watch({},(err,stats)=>{
     //获取serverBundle中的信息
     const bundlePath = path.join(serverConfig.output.path, serverConfig.output.filename);
     const bundle = mfs.readFileSync(bundlePath);
+    const m = new Module();
+    m._compile(bundle);//让module来解析我们内存获得的bundle
 });
 
 module.exports=function(app){
